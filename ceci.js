f548f4b30f536e8429d55a76a7c9091514f5c31d
eddwordd <edward.plasencio@gmail.com>
2023-12-29 21:13:06 -0500

    edwards edit

---
+++ ceci.js
@@ -0,0 +1,7 @@
+//  this is a fake function to show you how to work asyncronously
+
+// function fakeAsyncFunction(callback) {
+//     setTimeout(function() {
+//         callback("hello world");
+//     }, 1000);
+// }
